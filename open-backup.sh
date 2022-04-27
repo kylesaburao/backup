31c7bcc1726de745073ea09d3b79aeb59398a0b2
Kyle Saburao <ksaburao@sfu.ca>
2022-04-26 22:07:18 -0700

    update open script

--- open-backup.sh
+++ open-backup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 SCRIPT_DIR=$(dirname $0:A)
-gpg -d --passphrase-file $SCRIPT_DIR/auth.env --batch $1 | tar xvf -
+gpg -d --passphrase-file $SCRIPT_DIR/auth.env --batch $1 | tar xzvf -
